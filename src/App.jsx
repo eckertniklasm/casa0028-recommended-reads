3b35fcf1d8034bc16fbbbd51fc31c49941626aa5
ddunc23 <d.hay@ucl.ac.uk>
2026-01-22 15:38:07 +0000

    updated app shell

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TitleBar from './components/TitleBar'
+import MapDisplay from './components/MapDisplay'
+import './tw-styles.css'
+import PlaqueModal from './components/PlaqueModal'
 
 function App() {
 
-  const [count, setCount] = useState(0)
-
-  function increment() {
-    setCount(count + 1)
-  }
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={increment}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="mx-auto max-w-screen-xl bg-gray-50 min-h-screen">
+      <TitleBar title="Recommended Reading" />
+      <MapDisplay longitude={-0.137310} latitude={51.521699} />
+      {isModalOpen ? <PlaqueModal /> : null}
+  </div>
   )
 }
 
---
+++ src/components/MapDisplay.jsx
@@ -0,0 +1,18 @@
+import Map from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+function MapDisplay(props) {
+  return (
+    <Map
+      initialViewState={{
+        longitude: props.longitude || -122.4,
+        latitude: props.latitude || 37.8,
+        zoom: props.zoom || 10
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+    />
+  );
+}
+
+export default MapDisplay;
